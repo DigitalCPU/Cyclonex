cd7e9e08dcf268355ed9e899b023bd7d32fc7cef
DigitalCPU <135297237+DigitalCPU@users.noreply.github.com>
2025-02-23 13:01:23 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    if (sidebar.classList.contains("hidden")) {
+        sidebar.classList.remove("hidden");
+    } else {
+        sidebar.classList.add("hidden");
+    }
+}
